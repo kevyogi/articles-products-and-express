57b3e8d5143dbd9e403f6809c750578894bd102d
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-09 18:53:23 -1000

    fixed where the logic is done

--- db/products.js
+++ db/products.js
@@ -1,44 +1,46 @@
-const collection = [];
-let idNumber = 0;
-
 class Products {
 
-  create(req, res){
-    if(!req.body.name || !req.body.price || !req.body.inventory){
-      res.redirect('products/new');
-    }else{
-      collection.push({'id': (idNumber), 'name': req.body.name, 'price': Number(req.body.price), 'inventory': Number(req.body.inventory)});
-      idNumber++;
-      res.redirect('/products');
-      console.log(collection);
+  constructor(){
+    this._collection = [];
+    this._id = 0;
+  }
+
+  create(data){
+    this._collection.push({'id': (this._id), 'name': data.name, 'price': Number(data.price), 'inventory': Number(data.inventory)});
+    this._id++;
+    console.log(this._collection);
+  }
+
+  getById(reqURL){
+    let ID = Number((reqURL.split('/')[1]));
+    for(let i = 0; i < this._collection.length; i++){
+      if(this._collection[i].id === ID){
+        return this._collection[i];
+      }else if(i === this._collection.length-1){
+        return false;
+      }
     }
   }
 
-  edit(req, res){
-    let r = req.body;
-    for(let i = 0; i < collection.length; i++){
-      if(collection[i].id === Number(r.id)){
-        if(r.name){
-          collection[i].name = r.name;
-        }
-        if(r.price){
-          collection[i].price = Number(r.price);
-        }
-        if(r.inventory){
-          collection[i].inventory = Number(r.inventory);
-        }
-        res.redirect('/:id')
-      }else if(i === collection.length - 1){
-        res.redirect('/:id/edit')
-      }
+  edit(data, target){
+    console.log(this._collection);
+    if(data.name){
+      target.name = data.name;
     }
-    console.log(collection);
+    if(data.price){
+      target.price = data.price;
+    }
+    if(data.inventory){
+      target.inventory = data.inventory;
+    }
+    console.log(this._collection);
+    return target;
   }
 
   delete(req, res){
     let r = req.body;
     for(let i = 0; i < collection.length; i++){
-      if(collection[i].id === Number(r.id)){
+      if(collection[i].id === Number(req.param.id)){
         collection.splice(i, 1);
         console.log(collection);
         res.redirect('/products');
--- routes/products.js
+++ routes/products.js
@@ -6,11 +6,28 @@ const router = express.Router();
 // let productsArray = [];
 
 router.post('/', (req, res) => {
-  products.create(req, res);
+  const data = req.body;
+  if(!data.name || !data.price || !data.inventory){
+    res.redirect('products/new');
+  }else{
+    products.create(data);
+    res.redirect('/products');
+  }
 });
 
 router.put('/:id', (req, res) => {
-  products.edit(req, res);
+  const data = req.body;
+  const targetProduct = products.getById(req.url);
+  if(data.name || data.price || data.inventory){
+    if(targetProduct){
+      products.edit(data, targetProduct);
+      res.redirect(`/products/${targetProduct.id}`);
+    }else{
+      res.redirect(`/products${req.url}/edit`);
+    }
+  }else{
+    res.redirect(`/products${req.url}/edit`);
+  }
 });
 
 router.delete('/:id', (req, res) => {
