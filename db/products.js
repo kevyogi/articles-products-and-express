fbc87d8e777378530b18bb60128194fbb2be3a5e
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-09 16:36:06 -1000

    made basic funtionality for post, put, delete products

--- db/products.js
+++ db/products.js
@@ -1,14 +1,55 @@
 const collection = [];
+let idNumber = 0;
 
-function createDataObj(req, res){
-  if(!req.body.name || !req.body.price || !req.body.inventory){
-    res.redirect('/new');
-  }else{
-    collection.push({'id': (collection.length+1), 'name': req.body.name, 'price': Number(req.body.price), 'inventory': req.body.inventory});
+class Products {
 
+  create(req, res){
+    if(!req.body.name || !req.body.price || !req.body.inventory){
+      res.redirect('products/new');
+    }else{
+      collection.push({'id': (idNumber), 'name': req.body.name, 'price': Number(req.body.price), 'inventory': Number(req.body.inventory)});
+      idNumber++;
+      res.redirect('/products');
+      console.log(collection);
+    }
   }
+
+  edit(req, res){
+    let r = req.body;
+    for(let i = 0; i < collection.length; i++){
+      if(collection[i].id === Number(r.id)){
+        if(r.name){
+          collection[i].name = r.name;
+        }
+        if(r.price){
+          collection[i].price = Number(r.price);
+        }
+        if(r.inventory){
+          collection[i].inventory = Number(r.inventory);
+        }
+        res.redirect('/:id')
+      }else if(i === collection.length - 1){
+        res.redirect('/:id/edit')
+      }
+    }
+    console.log(collection);
+  }
+
+  delete(req, res){
+    let r = req.body;
+    for(let i = 0; i < collection.length; i++){
+      if(collection[i].id === Number(r.id)){
+        collection.splice(i, 1);
+        console.log(collection);
+        res.redirect('/products');
+      }else if(i === collection.length-1){
+        res.redirect('/products/:id');
+      }
+    }
+  }
+
 }
 
-module.exports = {
-  createDataObj: createDataObj
-}
+
+
+module.exports = Products
--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const Products = require('../db/products.js');
-let router = express.Router();
+const products = new Products
+const router = express.Router();
 
 // let productsArray = [];
 
 router.post('/', (req, res) => {
-  Products.createDataObj(req, res);
+  products.create(req, res);
 });
 
 router.put('/:id', (req, res) => {
+  products.edit(req, res);
+});
 
+router.delete('/:id', (req, res) => {
+  products.delete(req, res);
 });
 
 module.exports = router;
